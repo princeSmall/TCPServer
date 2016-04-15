9768ec4898a0ee325bf1f16fa37e86068ff51fe2
Tongle <Tongle@sansi.com>
2016-04-15 13:42:19 +0800

    Initial Commit

---
+++ TCPServer/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TCPServer
+//
+//  Created by tongle on 16/4/15.
+//  Copyright © 2016年 tongle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
